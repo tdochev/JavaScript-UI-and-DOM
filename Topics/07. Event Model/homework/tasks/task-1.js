8b9205995186d052b4d423a7cb9d5d4bf5ba7ba3
Tsvetomir Dochev <tddochev@gmail.com>
2017-03-12 19:05:56 +0200

    Added solution to event model homework

--- Topics/07. Event Model/homework/tasks/task-1.js
+++ Topics/07. Event Model/homework/tasks/task-1.js
@@ -1,4 +1,4 @@
-/* globals $ */
+/* globals module  */
 
 /* 
 
@@ -7,9 +7,39 @@ Create a function that takes an id or DOM element and:
 
 */
 
-function solve(){
+function solve() {
+  'use strict';
   return function (selector) {
+    if (typeof selector === 'string') {
+      selector = document.getElementById(selector);
+    }
+    if (!(selector instanceof HTMLElement)) {
+      throw new Error('Provided first parameter is neither string or existing DOM element!');
+    }
+    let items = selector.children;
+    for (let i = 0, len = items.length; i < len; i += 1) {
+      if (items[i].className === 'button') {
+        items[i].innerHTML = 'hide';
+        items[i].addEventListener('click', function(e) {
+          let topmostContentBeforeButton;
+          for (let y = i + 1; y <  items.length; y += 1) {
+            if (items[y+1] && items[y].className === 'content' && items[y + 1].className === 'button') {
+              topmostContentBeforeButton = items[y];
+              if (topmostContentBeforeButton.style.display === '') {
+                topmostContentBeforeButton.style.display = 'none'; 
+                e.target.innerHTML = 'show';
+
+              } else {
+                topmostContentBeforeButton.style.display = '';
+                e.target.innerHTML = 'hide';
+              }
+              break;
+            }
+          }
+        });
+      }
+    }
   };
-};
+}
 
 module.exports = solve;
