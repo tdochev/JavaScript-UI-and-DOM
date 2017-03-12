582cace53a17c19404af5d76cf15d53f61361219
Tsvetomir Dochev <tddochev@gmail.com>
2017-03-12 21:55:42 +0200

    Added solution to task 1 from jQuery overview

--- Topics/08. jQuery Overview/homework/tasks/task-1.js
+++ Topics/08. jQuery Overview/homework/tasks/task-1.js
@@ -1,7 +1,6 @@
-/* globals $ */
+/* globals $, module */
 
 /* 
-
 Create a function that takes a selector and COUNT, then generates inside a UL with COUNT LIs:   
   * The UL must have a class `items-list`
   * Each of the LIs must:
@@ -18,11 +17,26 @@ Create a function that takes a selector and COUNT, then generates inside a UL wi
         * Invalid COUNT values:
           * '123px' 'John', {}, [] 
 */
-
 function solve() {
+
+  'use strict';
   return function (selector, count) {
-   
+    if (!count || typeof count !== 'number' || count < 1) {
+      throw Error('Invalid count!');
+    }
+    if (typeof selector !== 'string') {
+      throw Error('Invalid selector');
+    }
+
+    let $selected = $(selector);
+    let $ul = $('<ul>').addClass('items-list');
+
+    for (let i = 0; i < count; i+=1) {
+      $('<li>').addClass('list-item').text('List item #' + i).appendTo($ul);
+      
+    }
+    $selected.append($ul);
   };
-};
+}
 
 module.exports = solve;
